a18983ed5f70893d63bab85f1c8a9c409ab5c864
swordcube <swordsoftarchive@gmail.com>
2023-09-09 17:58:38 -0500

    image example doesn't use  keycode
    oh wait i forgot to commit the examples woops

---
+++ examples/basic-image-test/src/Test.hx
@@ -0,0 +1,56 @@
+package;
+
+import sdl.Image;
+import sdl.Texture;
+import sdl.SDL;
+import sdl.Window;
+import sdl.Renderer;
+import sdl.Event;
+
+class Test {
+	static function main() {
+		SDL.init(InitFlags.VIDEO | InitFlags.AUDIO | InitFlags.EVENTS);
+
+		var window:Window = SDL.createWindow(
+			"SDL Test", 
+			WindowPos.CENTERED, WindowPos.CENTERED, 
+			1280, 720, 
+			WindowInitFlags.RESIZABLE | WindowInitFlags.ALLOW_HIGHDPI
+		);
+		var renderer:Renderer = SDL.createRenderer(window, -1, RenderFlags.ACCELERATED);
+
+		var running:Bool = true;
+		var event:Event = SDL.createEventPtr(); // workaround function for making an empty pointer
+
+		Image.init(ImageInitFlags.EVERYTHING);
+
+		var tex:Texture = Image.loadTexture(renderer, "assets/suffering.jpg");
+		var texSize:Point = SDL.getTextureSize(tex);
+
+		while(running) {
+			SDL.pollEvent(event);
+			final event:SDL_Event = event.ref;
+
+			SDL.setRenderDrawColor(renderer, 0, 0, 0, 0);
+			SDL.renderClear(renderer);
+			
+			SDL.renderCopy(renderer, tex, 
+				Rectangle.create(0, 0, texSize.x, texSize.y),
+				Rectangle.create(30, 30, texSize.x, texSize.y),
+			);
+			
+			SDL.renderPresent(renderer);
+
+			switch(event.type) {
+				case SDL_QUIT:
+					running = false;
+
+				default:
+			}
+		}
+
+		SDL.destroyRenderer(renderer);
+		SDL.destroyWindow(window);
+		SDL.quit();
+	}
+}
